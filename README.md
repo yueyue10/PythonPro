2c9e20c923ab32f55fcc97850b612749c3dd1185
zhaoyj <1650432983@qq.com>
2019-07-03 16:51:29 +0800

    更新readme文件

--- README.md
+++ README.md
@@ -5,10 +5,14 @@
 > * 1.安装Python应用并将其配置到环境变量中
 > * 2.在\configs\DbConfig.py程序中，配置项目连接的MySql数据库，
       在\configs\ComConf.py程序中配置日志保存路径
-> * 3.使用Intellij IDEA运行项目中的\spider\GithubSpider.py程序，
+> * 3.使用\doc\github_data.sql文件配置数据库的github_data表
+> * 4.使用Intellij IDEA运行项目中的\spider\GithubSpider.py程序，
       运行完成即可在查看数据库爬取到的数据
 
 <div>
-<h4>保存到数据库的数据-----------------</h4>
+<h5align="center">数据库配置</h5>
+<img src="https://github.com/yueyue10/PythonPro/raw/master/doc/pic_sql_design.png" width="100%"  />
+<img src="https://github.com/yueyue10/PythonPro/raw/master/doc/DbConfig.png" width="100%"  />
+<h5 align="center">查看保存到数据库的数据</h5>
 <img src="https://github.com/yueyue10/PythonPro/raw/master/doc/pic_sql.png" width="100%"  />
 </div>
---
+++ doc/github_data.sql
@@ -0,0 +1,28 @@
+﻿/*
+Navicat MySQL Data Transfer
+
+Source Server         : zhaoyj
+Source Server Version : 50721
+Source Host           : localhost:3306
+Source Database       : spider_2019
+
+Target Server Type    : MYSQL
+Target Server Version : 50721
+File Encoding         : 65001
+
+Date: 2019-07-03 16:36:05
+*/
+
+SET FOREIGN_KEY_CHECKS=0;
+
+-- ----------------------------
+-- Table structure for `github_data`
+-- ----------------------------
+DROP TABLE IF EXISTS `github_data`;
+CREATE TABLE `github_data` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `title` varchar(255) NOT NULL,
+  `description` varchar(255) DEFAULT NULL,
+  `news_url` varchar(255) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=155 DEFAULT CHARSET=utf8mb4;
